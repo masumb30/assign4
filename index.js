0b6a6106052be3957980161ccca8d6b46c6612e7
masumb30 <masum30.b@gmail.com>
2026-02-23 00:35:13 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
+const allJobContainer = document.getElementById('alljob-container');
 const interviewJobContainer = document.getElementById('interview-container');
-const rejectedJobContainer = document.getElementById('rejected-container')
+const rejectedJobContainer = document.getElementById('rejected-container');
+const totalJobCount = document.querySelectorAll('.total-job-count');
 let interviewJobsArray = [];
 let rejectedJobsArray = [];
 
@@ -23,13 +25,36 @@ function renderRejectedSection(){
     }
 }
 
+// job delete function
+
+allJobContainer.addEventListener('click', function(event){
+    console.log(event.target);
+    if(event.target.classList.contains('delete')){
+        const name = event.target.parentNode.children[0].innerText;
+        
+        const parent = event.target.parentNode.parentNode;
+        allJobContainer.removeChild(parent);
+        
+        totalJobCount.forEach(count=> count.innerText = Number(count.innerText) -1)
+        // get the job name and filter the interview array and the rejected array
+
+    }
+    if(event.target.classList.contains('interview-btn')){
+        // extract the information 
+        const jobcard = event.target.parentNode.parentNode;
+        const company = jobcard.children[0].children[0].innerText;
+        console.log('company', company);
+        const jobTitle = jobcard.children[1].innerText;
+        console.log("jobtitle: ", jobTitle)
+    }
+})
+
 // code to switch tabs
 
 // onclick tabs change their colors
 const allJobsButton = document.getElementById('allJobs');
 const interviewJobsButton = document.getElementById('interviewJobs');
 const rejectedJobsButton = document.getElementById('rejectedJobs');
-const allJobContainer = document.getElementById('alljob-container');
 
 allJobsButton.addEventListener('click', function(){
     interviewJobsButton.classList.remove('bg-blue-500');
